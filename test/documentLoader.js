841c638aa35adb742cecfbc695857a8bba62b862
Dave Longley <dlongley@digitalbazaar.com>
2024-08-26 11:05:21 -0400

    Add vc-di-ecdsa test vectors.

--- test/documentLoader.js
+++ test/documentLoader.js
@@ -1,16 +1,33 @@
 /*!
- * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Digital Bazaar, Inc. All rights reserved.
  */
+import * as didMethodKey from '@digitalbazaar/did-method-key';
+import * as EcdsaMultikey from '@digitalbazaar/ecdsa-multikey';
 import {
   controllerDocEcdsaMultikey,
   ecdsaMultikeyKeyPair,
   mockPublicEcdsaMultikey,
 } from './mock-data.js';
+import {CachedResolver} from '@digitalbazaar/did-io';
 import dataIntegrityContext from '@digitalbazaar/data-integrity-context';
 import multikeyContext from '@digitalbazaar/multikey-context';
 import {securityLoader} from '@digitalbazaar/security-document-loader';
 
 export const loader = securityLoader();
+
+const resolver = new CachedResolver();
+const didKeyDriver = didMethodKey.driver();
+console.log('didKeyDriver', didKeyDriver);
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'zDna',
+  fromMultibase: EcdsaMultikey.from
+});
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'z82L',
+  fromMultibase: EcdsaMultikey.from
+});
+resolver.use(didKeyDriver);
+loader.setDidResolver(resolver);
 
 loader.addStatic(
   ecdsaMultikeyKeyPair.controller,
@@ -30,3 +47,11 @@ loader.addStatic(
   multikeyContext.constants.CONTEXT_URL,
   multikeyContext.contexts.get(multikeyContext.constants.CONTEXT_URL)
 );
+
+loader.addStatic(
+  'https://www.w3.org/ns/credentials/examples/v2',
+  {
+    '@context': {
+      '@vocab': 'https://www.w3.org/ns/credentials/examples#'
+    }
+  });
